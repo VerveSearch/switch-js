24dbbfa1736d3ad36a079422b60d924031815859
anastasiatim <anastaiatim@yandex.ru>
2015-07-28 15:54:58 +0100

    Version 0.1.0

--- dist/switch-js.js
+++ dist/switch-js.js
@@ -5,13 +5,16 @@
 		var self= this;
 		self.element= el;
 		self.className = opts.className || 'active';
+		self.containerClassName = opts.containerClassName || 'active';
 		self.activeIndex = opts.activeIndex || 0;
 		self.label1 = opts.labels.label1 || null;
 		self.labelEl1 =  null;
 		self.labelEl2 = null;
-		self.label2 = opts.labels.label2 || null;		
-		self.el1 = opts.elementFirst || el.attr('data-target-switch1-content');
-		self.el2 = opts.elementSecond || el.attr('data-target-switch2-content');		
+		self.label2 = opts.labels.label2 || null;
+		var sel1 = opts.elementFirst || el.attr('data-target-switch1-content'),
+			sel2 = opts.elementSecond || el.attr('data-target-switch1-content'); 
+
+		self.contentEls = $([$(sel1).get(0),$(sel2).get(0)]);		
 		self.init();
 	};
 
@@ -29,39 +32,24 @@
 		},
 		onItemClick:function(item){ 
 		    var self= this;
-		 	$(item.target).siblings().removeClass(self.className);
-		 	$(item.target).addClass(self.className);
-		 	if($(item.target).hasClass('switch-state-1')){
-				$(self.el1).show();
-				$(self.el2).hide();
-		 	} else {
-		 		$(self.el2).show();
-				$(self.el1).hide();
-		 	}
+		    var itemEl = $(item.target);
+		    var index = self.activeIndex = itemEl.siblings()
+		    	  .removeClass(self.className)
+		    	  .end()
+		    	  .addClass(self.className)
+		    	  .index();
+			self.contentEls.removeClass(self.containerClassName).eq(self.activeIndex).addClass(self.containerClassName);
 		},
 		render: function(){
-			var self= this;
-				self.labelEl1 = self.element.find('.switch-state-1');
-				self.labelEl2 = self.element.find('.switch-state-2'); 
+			var self= this, labelEl1, labelEl2;
 			if(this.element.children().length === 0){
 				this.element
-					.append($('<span class="switch-state-1">'+sel.label1+'</span>'))
-					.append($('<span class="switch-state-2">'+self.label2+'</span>'));
-				 self.labelEl1 = self.element.find('.switch-state-1');
-				self.labelEl2 = self.element.find('.switch-state-2');
-			}else {
-				self.labelEl1.html(self.label1);
-				self.labelEl2.html(self.label2);
-			}
-			if(self.activeIndex === 0){
-				self.labelEl1.addClass(self.className);
-				$(self.el1).show();
-				$(self.el2).hide();
-			} else {
-				self.labelEl2.addClass(self.className);
-				$(self.el1).hide();
-				$(self.el2).show();
-			}
+					.append(['<span class="switch-state-1">'+self.label1+'</span>',
+					'<span class="switch-state-2">'+self.label2+'</span>'].join(''));
+			} 
+			labelEl1 = self.element.find('.switch-state-1').html(self.label1);
+			labelEl2 = self.element.find('.switch-state-2').html(self.label2);
+			this.contentEls.removeClass(self.containerClassName).eq(self.activeIndex).addClass(self.containerClassName);
 			this.element.children().on('click', $.proxy(this.onItemClick,this));
 		}
 	};
